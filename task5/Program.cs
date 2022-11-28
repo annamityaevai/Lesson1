eb09648762638ed27d48f681775ba9b790566b1e
Anna Mityaeva <aiisa@inbox.ru>
2022-11-28 09:06:28 +0300

    added task 5

---
+++ task5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Insert a three digit number");
+int a = Convert.ToInt32(Console.ReadLine());
+if ((1000 > a) && (a > 99)){
+    Console.WriteLine("Last digit is " + a%10);
+}
+else
+{
+    Console.WriteLine("Error. Please insert a three digit number!");
+}
