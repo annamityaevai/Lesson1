caffb16d9888997dca99b8d98abd8085701c333d
Anna Mityaeva <aiisa@inbox.ru>
2022-11-28 08:03:43 +0300

    added task 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Input number");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("N * N = " + N * N);
