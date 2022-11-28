6f453d4c37eb00766a7bc5fb573634b6d3c48c4a
Anna Mityaeva <aiisa@inbox.ru>
2022-11-28 08:20:26 +0300

    added task 2

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Input number a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a == b * b)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
